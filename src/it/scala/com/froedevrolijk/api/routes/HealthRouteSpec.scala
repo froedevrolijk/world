77f494c1d9276f74be0f7a6c953d4f4d9b1c0812
Froede Vrolijk <froede.vrolijk@ing.com>
2021-04-15 15:28:26 +0200

    wip

--- src/it/scala/com/froedevrolijk/api/routes/HealthRouteSpec.scala
+++ src/it/scala/com/froedevrolijk/api/routes/HealthRouteSpec.scala
@@ -1,20 +1,20 @@
-package com.froedevrolijk.api.routes
-
-import org.http4s.{ Method, Status }
-import org.specs2.mutable.Specification
-
-class HealthRouteSpec extends Specification with ApiRoutes {
-
-  private val path = "/keepalive"
-
-  "HealthRoute" should {
-    "return OK for a healthy endpoint" >>
-    routes
-      .use { implicit r =>
-        for {
-          response <- createRequest(Method.GET, path).compile
-        } yield response.status must beEqualTo(Status.Ok)
-      }
-      .unsafeRunSync()
-  }
-}
+//package com.froedevrolijk.api.routes
+//
+//import org.http4s.{ Method, Status }
+//import org.specs2.mutable.Specification
+//
+//class HealthRouteSpec extends Specification with ApiRoutes {
+//
+//  private val path = "/keepalive"
+//
+//  "HealthRoute" should {
+//    "return OK for a healthy endpoint" >>
+//    routes
+//      .use { implicit r =>
+//        for {
+//          response <- createRequest(Method.GET, path).compile
+//        } yield response.status must beEqualTo(Status.Ok)
+//      }
+//      .unsafeRunSync()
+//  }
+//}
